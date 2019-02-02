64b17585da30354a78d598a81471d7122e36ead2
codedecde <barunpatra95@gmail.com>
2019-02-01 22:35:38 -0800

    bug fix

--- AttentionSegmentation/model/__init__.py
+++ AttentionSegmentation/model/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
-from AttentionSegmentation.model.attention_module import DotAttention, BaseAttention, KeyedAttention
+from AttentionSegmentation.model.attention_module import \
+    DotAttention, BaseAttention, KeyedAttention, GatedAttention
 # from AttentionSegmentation.model.classifiers import Classifier
 # from AttentionSegmentation.model.attn_classifier import *
 # from AttentionSegmentation.model.dyn_hier_attn_classifier import *
