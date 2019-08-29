1886f43fdbb756cd82017ca278a0afe7f133acf9
codedecde <barunpatra95@gmail.com>
2019-08-29 15:37:38 -0400

    get bert embeddings easily

--- get_embeddings.sh
+++ get_embeddings.sh
@@ -25,6 +25,14 @@ case $i in
 esac
 done
 
+# Function for creating the embedding folder
+function make_embed_dir () {
+	local embed_dir=$1
+	if [ ! -d ${embed_dir} ]; then
+		mkdir -p ${embed_dir}
+		touch ${embed_dir}/.gitkeep
+	fi
+}
 # Get embeddings based on parameters passed
 if [ ${EMBED_TYPE} = "glove" ]; then
 	echo ${EMBED_DIM}
@@ -34,24 +42,18 @@ if [ ${EMBED_TYPE} = "glove" ]; then
 	EMBED_FILE=glove.6B.${EMBED_DIM}d.txt.gz
     EMBED_SRC=https://s3-us-west-2.amazonaws.com/allennlp/datasets/glove/${EMBED_FILE}
 	EMBED_DIR=${EMBED_BASE_DIR}/GloveEmbeddings
-	if [ ! -d ${EMBED_DIR} ]; then
-		mkdir ${EMBED_DIR}
-		touch ${EMBED_DIR}/.gitkeep
-	fi
+	make_embed_dir ${EMBED_DIR}
 	DEST_EMBED_PATH=${EMBED_DIR}/${EMBED_FILE}
 	echo "${DEST_EMBED_PATH}"
 	if [ ! -f ${DEST_EMBED_PATH} ]; then
 		echo "Downloading ${EMBED_FILE}"
 	 	wget -O ${DEST_EMBED_PATH} ${EMBED_SRC}
 	fi
-elif [ ${EMBED_TYPE}="elmo" ]; then
+elif [ ${EMBED_TYPE} = "elmo" ]; then
 	OPT_SRC=https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_options.json
 	WEIGHTS_SRC=https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5
 	EMBED_DIR=${EMBED_BASE_DIR}/ELMOEmbeddings
-	if [ ! -d ${EMBED_DIR} ]; then
-		mkdir ${EMBED_DIR}
-		touch ${EMBED_DIR}/.gitkeep
-	fi
+	make_embed_dir ${EMBED_DIR}
 	OPT_FILE=${EMBED_DIR}/"elmo_2x4096_512_2048cnn_2xhighway_options.json"
 	WEIGHTS_FILE=${EMBED_DIR}/"elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
 	if [ ! -f ${OPT_FILE} ]; then
@@ -61,6 +63,24 @@ elif [ ${EMBED_TYPE}="elmo" ]; then
 	if [ ! -f ${WEIGHTS_FILE} ]; then
 		echo "Downloading ${WEIGHTS_FILE}"
 		wget -O ${WEIGHTS_FILE} ${WEIGHTS_SRC}
+	fi
+elif [ ${EMBED_TYPE} = "bert" ]; then
+	BERT_VOCAB="https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-multilingual-cased-vocab.txt"
+	BERT_MODEL="https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-multilingual-cased.tar.gz"
+	MODEL_NAME="bert-base-multilingual-cased"
+	EMBED_DIR=${EMBED_BASE_DIR}/BERTEmbeddings/${MODEL_NAME}
+	make_embed_dir ${EMBED_DIR}
+	VOCAB_FILE=${EMBED_DIR}/${BERT_VOCAB}
+	MODEL_FILE=${EMBED_DIR}/${BERT_MODEL}
+	VOCAB_OUTPUT_FILE=${EMBED_DIR}/vocab.txt
+	MODEL_OUTPUT_FILE=${EMBED_DIR}/${MODEL_NAME}.tar.gz
+	if [ ! -f ${VOCAB_OUTPUT_FILE} ]; then
+		echo "Downloading ${VOCAB_OUTPUT_FILE}"
+		wget -O ${VOCAB_OUTPUT_FILE} ${BERT_VOCAB}
+	fi
+	if [ ! -f ${MODEL_OUTPUT_FILE} ]; then
+		echo "Downloading ${MODEL_OUTPUT_FILE}"
+		wget -O ${MODEL_OUTPUT_FILE} ${BERT_MODEL}
 	fi
 else
 	echo "TYPE ${EMBED_TYPE} unrecognized"
